32720a0e3a31a75b2623f30e121e8aec6bb3dee0
spieriz <spieriz@gmail.com>
2019-04-19 19:16:38 +0200

    Added return type of autoloader methods.

--- src/CustomAutoloader.php
+++ src/CustomAutoloader.php
@@ -5,13 +5,13 @@ spl_autoload_register('CustomAutoloader::loadAlgorithms');
 
 class CustomAutoloader
 {
-    public static function loadAlgorithms($class_name){
+    public static function loadAlgorithms($class_name): void {
         $path = 'Algorithms/';
 
         spl_autoload($path . '/' . $class_name);
     }
 
-    public static function loadStartupClasses($class_name){
+    public static function loadStartupClasses($class_name): void {
         spl_autoload($class_name);
     }
 }
