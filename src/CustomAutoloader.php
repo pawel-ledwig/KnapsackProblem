a1bda02af5566c4db7188a225e0f46a196da4522
spieriz <spieriz@gmail.com>
2019-04-19 19:55:54 +0200

    Updated autoloader: added loading of exceptions.

--- src/CustomAutoloader.php
+++ src/CustomAutoloader.php
@@ -1,7 +1,9 @@
 <?php
 
 
+spl_autoload_register('CustomAutoloader::loadStartupClasses');
 spl_autoload_register('CustomAutoloader::loadAlgorithms');
+spl_autoload_register('CustomAutoloader::loadExceptions');
 
 class CustomAutoloader
 {
@@ -13,5 +15,11 @@ class CustomAutoloader
 
     public static function loadStartupClasses($class_name): void {
         spl_autoload($class_name);
+    }
+
+    public static function loadExceptions($class_name): void {
+        $path = 'Exceptions/';
+
+        spl_autoload($path . '/' . $class_name);
     }
 }
