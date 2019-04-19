0bb22d9149f93590d2ea7bc46ecf6f7e6a56e567
spieriz <spieriz@gmail.com>
2019-04-19 20:32:18 +0200

    Added method to load and parse data from file to an array.

--- src/FileLoader.php
+++ src/FileLoader.php
@@ -6,7 +6,40 @@ class FileLoader
     private $filepath;
     private $items_data;
 
-    public function __construct($filepath){
+    public function __construct($filepath)
+    {
         $this->filepath = $filepath;
+    }
+
+    /**
+     * Used to load data from file and put them into an array.
+     * @throws FileNotFoundException
+     */
+    public function loadDataFromFile(): void
+    {
+        if (!file_exists($this->filepath)) {
+            throw new FileNotFoundException($this->filepath);
+        }
+
+        $file_handler = fopen($this->filepath, "r");
+
+        // Skip first line of a file (header with column names)
+        fgets($file_handler);
+
+        // For each next line of a file
+        while (!feof($file_handler)) {
+            $line = fgets($file_handler);
+            $array = explode(";", $line);
+
+            // Check if CSV line has all required columns
+            if (sizeof($array) === CSV_COLUMNS) {
+                $this->items_data[$array[0]] = array(
+                    'weight' => intval($array[1]),
+                    'value' => intval($array[2]),
+                );
+            } else {
+                echo "Malformed csv line: $line\nProceeding with next line.\n";
+            }
+        }
     }
 }
