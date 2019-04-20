7a2321465dc0c4e53403e4df10db78d8f8690b91
spieriz <spieriz@gmail.com>
2019-04-20 15:41:08 +0200

    Added init and getController methods. Removed argc variable.

--- src/CLI.php
+++ src/CLI.php
@@ -7,6 +7,8 @@ class CLI
     private $capacity;
     private $algorithm;
 
+    private $params;
+
     private $options = array(
         'file' => true, // filename
         'capacity' => true, // capacity
@@ -21,11 +23,22 @@ class CLI
         'h' => 'help',
     );
 
-    public function parseParams($argc, $argv)
+    public function init($argv)
+    {
+        $this->params = $argv;
+        $this->parseParams();
+    }
+
+    public function getController()
+    {
+        return new Controller($this->filename, $this->capacity, $this->algorithm);
+    }
+
+    private function parseParams()
     {
         // starting from $i = 1 because argv[0] is current path
-        for ($i = 1; $i < $argc; $i++) {
-            $arg = $argv[$i];
+        for ($i = 1; $i < sizeof($this->params); $i++) {
+            $arg = $this->params[$i];
 
             // check if argument is an option
             if ($arg[0] == '-') {
@@ -35,7 +48,7 @@ class CLI
                 if ($this->isOptionSupported($arg_name)) {
                     $option_name = $this->getOptionName($arg_name);
 
-                    $this->parseOption($argc, $argv, $option_name, $i);
+                    $this->parseOption($this->params, $option_name, $i);
                 } else {
                     // TODO: Command does not exists
                 }
@@ -53,15 +66,15 @@ class CLI
         return strlen($arg_name) == 1 ? $this->shortcuts[$arg_name] : $arg_name;
     }
 
-    private function parseOption(int $argc, array $argv, string $option_name, int $i): void
+    private function parseOption(array $params, string $option_name, int $i): void
     {
         // if there is no more arguments but option value is required
-        if ($i + 1 >= $argc && $this->options[$option_name]) {
+        if ($i + 1 >= sizeof($params) && $this->options[$option_name]) {
             // TODO: Missing argument for option $option_name
             return;
         }
 
-        $value = $argv[$i + 1];
+        $value = $params[$i + 1];
 
         // if there is an option in place of value of previous option, but value of previous option is required
         if ($value[0] == '-' && $this->options[$option_name]) {
