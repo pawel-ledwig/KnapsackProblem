35c2dd9e4c5fee0be3a3c9c4c3127467ed753ff7
spieriz <spieriz@gmail.com>
2019-04-20 18:46:15 +0200

    Added content of an index file.

--- src/index.php
+++ src/index.php
@@ -1,4 +1,18 @@
 <?php
 
 
+define('CSV_COLUMNS', 3);
 spl_autoload('CustomAutoloader');
+
+$cli = new CLI();
+$cli->init($_SERVER['argv']);
+
+$controller = $cli->getController();
+$controller->run();
+
+$knapsack = $controller->getKnapsack();
+
+if (!is_null($knapsack))
+    echo $knapsack->toString();
+else
+    echo "Check an error messages and run program again.\n";
