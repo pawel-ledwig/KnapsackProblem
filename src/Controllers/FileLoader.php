4515d8b5f78e0eb5ae3fda842900c769e89da720
spieriz <spieriz@gmail.com>
2019-04-23 17:04:10 +0200

    Added slash before relative file path.

--- src/Controllers/FileLoader.php
+++ src/Controllers/FileLoader.php
@@ -20,7 +20,7 @@ class FileLoader
 
     public function __construct($filepath)
     {
-        $this->filepath = getcwd() . "$filepath";
+        $this->filepath = getcwd() . "/$filepath";
     }
 
     /**
